bb6b83a57e2ea01086a038396dbc9271116b0904
Athul <akkv.2507@gmail.com>
2024-12-29 13:36:29 +0530

    Graph Added

--- Graph/main.go
+++ Graph/main.go
@@ -28,14 +28,61 @@ func (g *Graph) Print() {
 	}
 }
 
+func (g *Graph) BFS(start int) {
+	seen := make(map[int]bool)
+	queue := []int{}
+	queue=append(queue, start)
+	seen[start] = true
+	for len(queue) > 0 {
+		vertex := queue[0]
+		queue = queue[1:]
+		fmt.Printf("%d ", vertex)
+		for _, neighbour := range g.List[vertex] {
+			if !seen[neighbour] {
+				seen[neighbour] = true
+				queue = append(queue, neighbour)
+			}
+		}
+	}
+	fmt.Println()
+}
+
+func (g *Graph) DFS(start int){
+	seen:=make(map[int]bool)
+	stack:=[]int{start}
+	for len(stack)>0{
+		vertex:=stack[len(stack)-1]
+		stack=stack[:len(stack)-1]
+		if !seen[vertex]{
+			seen[vertex]=true
+			fmt.Printf("%d ",vertex)
+			for _, neighbours:= range g.List[vertex]{
+				if !seen[neighbours]{
+					stack=append(stack, neighbours)
+				}
+			}
+		}
+	}
+	fmt.Println()
+}
+
 func main() {
 	graph := &Graph{}
+	
 	graph.AddVertex(1)
 	graph.AddVertex(2)
 	graph.AddVertex(3)
 	graph.AddVertex(4)
-	graph.AddEdge(1,5)
-	graph.AddEdge(2,4)
-	graph.AddEdge(3,1)
-	graph.Print()
+	graph.AddVertex(5)
+
+
+	graph.AddEdge(1, 5)
+	graph.AddEdge(2, 4)
+	graph.AddEdge(3, 1)
+
+
+	fmt.Print("BFS: ")
+	graph.BFS(1)
+	fmt.Print("DFS: ")
+	graph.DFS(1)
 }
